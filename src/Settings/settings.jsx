1c595976a23ae809fb7b4eb7e7c7088cb2edec74
DanielleChason <danielle.chason@gmail.com>
2023-04-19 23:57:25 +0300

    Add new features

--- src/Settings/settings.jsx
+++ src/Settings/settings.jsx
@@ -1,42 +1,68 @@
+import {Func} from '../Building/building';
+import { useEffect, useState} from 'react';
 
 export const Settings = (props) => {
     function handleClick() {
         props.handleSet();
     }
-    
-    return (
-        <div className="settingsContainer">
-        <div>
-            <p>Elevators:
-            <input 
-                type="number"
-                className="input"
-                name="Amount of Elevators"
-                min="1"
-                value={props.elevatorsCount}
-                onChange={(e) => props.setElevatorsCount(Number(e.target.value))}
-            />
-            </p>
-            
-        </div>
-        <div>
-            <p>&nbsp;&nbsp;Floors:&nbsp;&nbsp;
-            <input 
-                type="number"
-                className="input"
-                min="1"
-                value={props.floorsCount}
-                onChange={(e) => props.setFloorsCount(Number(e.target.value))}
-            />
-            </p>
- 
-        </div>
-        <div>
-            <button className='enterButton' onClick={handleClick}>
-            Enter Building
-            </button>
-        </div>
 
+    const [allFloors, setAllFloors] = useState(document.getElementsByClassName('floorButton'));
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        let tempFloors = [...allFloors];
+        const toClick = tempFloors[Math.floor(Math.random() * (16 - (0) + 1)) + 0];
+        toClick.click();
+        setAllFloors(tempFloors);
+      }, 500);
+      return () => clearInterval(interval);
+    },);
+  
+
+        return (
+        <div>
+            <div className='elevatorPeak'>
+                {<Func
+                    elevatorsCount={15} 
+                    floorsCount={17} 
+                />}
+            </div>
+            
+
+            <div className="settingsContainer">
+        
+                <div>
+                    <p>Elevators:
+                    <input 
+                        type="number"
+                        className="input"
+                        name="Amount of Elevators"
+                        min="1"
+                        value={props.elevatorsCount}
+                        onChange={(e) => props.setElevatorsCount(Number(e.target.value))}
+                    />
+                    </p>
+                    
+                </div>
+                <div>
+                    <p>&nbsp;&nbsp;Floors:&nbsp;&nbsp;
+                    <input 
+                        type="number"
+                        className="input"
+                        min="1"
+                        value={props.floorsCount}
+                        onChange={(e) => props.setFloorsCount(Number(e.target.value))}
+                    />
+                    </p>
+
+                </div>
+                <div>
+                    <button className='enterButton' onClick={handleClick}>
+                    Enter Building
+                    </button>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/Utilities/utilities.jsx
+++ src/Utilities/utilities.jsx
@@ -1,10 +1,12 @@
 export const getFloorName = (index) => {
-    let toCheck = index % 100;
-    let suffix = index + 'th';
-    if (toCheck === 0) {
-        suffix = 'Ground Floor'
+    if (index === 0) {
+        return 'Ground Floor';
     }
-    else if (toCheck % 10 === 1 && toCheck % 100 !== 11) {
+
+    let toCheck = index % 100;
+    let suffix = index + 'th'
+    
+    if (toCheck % 10 === 1 && toCheck % 100 !== 11) {
         suffix = index + 'st';
     }
     else if (toCheck % 10 === 2 && toCheck % 100 !== 12) {
